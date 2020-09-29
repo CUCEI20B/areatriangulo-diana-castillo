11fade90cb76e78fd1eb1557dda5c8f0f06fc653
Diana Castillo <diana.castillo7676@alumnos.udg.mx>
2020-09-29 09:35:27 -0500

    prueba01

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    unsigned int base;
+    unsigned int altura;
+
+    cout <<(base*altura)/2
 
     return 0;
 }
